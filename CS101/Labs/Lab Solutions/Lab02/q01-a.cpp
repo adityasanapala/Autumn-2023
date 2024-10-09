7ff37a724ce9ba191889e7b43e93bcd3e1ea3a01
Aditya Sanapala <wickedsan106@gmail.com>
2024-10-10 02:36:09 +0530

    Update and rename q01.cpp to q01-a.cpp

---
+++ CS101/Labs/Lab Solutions/Lab02/q01-a.cpp
@@ -0,0 +1,14 @@
+#include <simplecpp>
+
+main_program{
+  int a, d, n;
+	cin >> a >> d >> n;
+	int term = a;
+	int multiplier = 0;
+	repeat(n) {
+    term = a + multiplier*d;
+		multiplier++;
+		cout << term << " ";
+	}
+	cout << '\n';
+}
