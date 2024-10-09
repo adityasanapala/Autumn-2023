c09ed1da869b3178cd2ed337e1405abcc87cf7ef
Aditya Sanapala <wickedsan106@gmail.com>
2024-10-10 02:39:53 +0530

    Create q01-b.cpp

---
+++ CS101/Labs/Lab Solutions/Lab02/q01-b.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, r, n;
+    cin >> a >> r >> n;
+    int term = a;
+
+    for (int i = 0; i < n; i++) {
+        cout << term << " ";
+        term = term * r;
+    }
+}
