9d8098a693d487505d77498d1bb226013e828cfe
Aditya Sanapala <wickedsan106@gmail.com>
2024-12-27 17:49:56 +0530

    Update q02.cpp

--- CS101/Labs/Lab Solutions/Lab02/q02.cpp
+++ CS101/Labs/Lab Solutions/Lab02/q02.cpp
@@ -2,4 +2,22 @@
 using namespace std;
 
 int main() {
+    int d;
+
+    cin >> d;
+
+    int z, y, x, w, v, temp;
+
+    z = d % 12;
+    temp = d / 12;
+    y = temp % 3;
+    temp /= 3;
+    x = temp % 220;
+    temp /= 220;
+    w = temp % 8;
+    v = temp / 8;
+
+    cout << d << " inches is actually " << v << " mi, " << w << " furlongs, " << x << " yards, " << y << " feet, " << z << " inches." << endl;
+    
+    return 0;
 }
